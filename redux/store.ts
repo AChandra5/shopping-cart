615bf74696be92cb48c558c278919c4309418b9f
Akhil chandra <akhilchandra2309@gmail.com>
2024-05-08 22:06:55 +0530

    setting up saga

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,14 @@
 import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { dummyReducer } from "./reducer";
-import AddToCart from "./action";
+import createSagaMiddleWare from "redux-saga";
+import productSaga from "./productSaga";
 
-
+const sagaMiddleWare = createSagaMiddleWare(); //create saga middleware
+const middlewares = [sagaMiddleWare]; //create middlewares array to add further middlewares like saga.
 export const store = configureStore({
-reducer:    {
-    reducer1: dummyReducer,
-},
+  reducer: dummyReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewares), // Include default middleware
 });
+sagaMiddleWare.run(productSaga);
